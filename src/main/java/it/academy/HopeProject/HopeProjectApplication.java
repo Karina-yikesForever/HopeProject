44586004a03f540642349399c0c5bc6f446a4274
Karina-YikesForever <karinakokosova8@gmail.com>
2020-06-12 00:47:45 +0600

    Initial commit

---
+++ src/main/java/it/academy/HopeProject/HopeProjectApplication.java
@@ -0,0 +1,13 @@
+package it.academy.HopeProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HopeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/academy/HopeProject/HopeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package it.academy.HopeProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
